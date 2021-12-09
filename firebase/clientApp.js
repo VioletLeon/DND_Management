673136fc3a5231ca8592f7bf7f938802b5f6c029
violettebunny <52981973+violettebunny@users.noreply.github.com>
2021-12-09 15:15:31 -0500

    base project for DND management site

---
+++ firebase/clientApp.js
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASEAPIKEY,
+  authDomain: process.env.FIREBASEAUTHDOMAIN,
+  projectId: process.env.FIREBASEPROJECTID,
+  storageBucket: process.env.FIREBASESTORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASEMESSAGINGSENDERID,
+  appId: process.env.FIREBASEAPPID,
+  measurementId: process.env.FIREBASEMEASUREMENTID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+export default {
+  getFirestore,
+  getAuth,
+  auth,
+  createUserWithEmailAndPassword,
+};
---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    FIREBASEAPIKEY: 'AIzaSyDf0lthyU8DjXroU3wl3UaHRZsD5_ojhCg',
+    FIREBASEAUTHDOMAIN: 'dnd-management.firebaseapp.com',
+    FIREBASEPROJECTID: 'dnd-management',
+    FIREBASESTORAGEBUCKET: 'dnd-management.appspot.com',
+    FIREBASEMESSAGINGSENDERID: 1021479350870,
+    FIREBASEAPPID: '1:1021479350870:web:2081fee5d2c24abcce01f7',
+    FIREBASEMEASUREMENTID: '${config.measurementId}',
+  },
+};
